00d4c8a6e23c412201f85b5f779e9da26d7ef7af
Ramoth <ramothlp@gmail.com>
2015-07-08 09:32:59 +0200

    Create MyDealz-Remove-Topbar.user.js

---
+++ MyDealz-Remove-Topbar.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         MyDealz Topbar ausblenden
+// @namespace    http://www.mydealz.de
+// @version      1.0
+// @description  Ausblendung der Topbar
+// @author       Ramoth
+// @include      http://www.mydealz.de/*
+// @include      https://www.mydealz.de/*
+// @grant        none
+// ==/UserScript==
+
+$(".topBar").hide();
